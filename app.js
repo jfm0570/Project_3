ab22ba5fe622e34e6886124ca795c2f5303c7951
Jenipher Flores Martinez <131614749+jfm0570@users.noreply.github.com>
2023-08-23 22:32:58 -0500

    Map added

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 
-// Project #3
-// Add url
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+// // Project #3
+// // Add url
+// const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// Promise Pending
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
+// // Promise Pending
+// const dataPromise = d3.json(url);
+// console.log("Data Promise: ", dataPromise);
+
+
+
+function createMap() {
+    // Create the tile layer that will be the background of our map.
+    let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+  
+    // Create a baseMaps object to hold the streetmap layer.
+    let baseMaps = {
+      "Street Map": streetmap
+    };
+  
+    
+    // Create the map object with options.
+    let map = L.map("map", {
+      center: [37.8, -96],
+      zoom: 5,
+      layers: [streetmap]
+    });
+
+  }
+
+createMap()
