6e7b02e75a23add1b3e79e751f027c1a720369a9
Jenipher Flores Martinez <131614749+jfm0570@users.noreply.github.com>
2023-08-24 22:10:55 -0500

    Added more details

--- app.js
+++ app.js
@@ -28,4 +28,25 @@ function createMap() {
 
   }
 
+  function optionChanged(id){
+    // let dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a letiable
+    // let id = dropdownMenu.property("value");
+    // create_bar_graph(final_data, id)
+    // create_bubble_graph(final_data,id)
+    // id_selected = id
+    // deleteRows()
+    // demographics(data_full,id_selected)
+  }
+
+  // Plot to add data for temperature vs month for each city
+  function Temperature_chart() {
+    data = [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16] }];
+  
+    Plotly.newPlot("Temperature_plot", data);
+  }
+
 createMap()
+Temperature_chart()
